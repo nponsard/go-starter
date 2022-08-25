9fb018ad77da872176f518d44f1169dbb5c19e89
Nils Ponsard <nilsponsard@gmail.com>
2022-08-25 11:20:49 +0200

    finish bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-MOD_URI="test"
+MOD_URI="$1"
 MOD_NAME=$(basename $MOD_URI)
 
+if [ -z "$MOD_URI" ]; then
+    echo "Usage: $0 <mod-uri>"
+    exit 1
+fi
 
-files=(./main.go,./.env,./internal/commands/commands.go, ./internal/commands/ping/ping.go, ./.gitignore, ./go.mod, ./Makefile )
+files=(./main.go ./.env ./internal/commands/commands.go  ./internal/commands/ping/ping.go  ./.gitignore  ./go.mod  ./Makefile )
 
 for file in "${files[@]}"
 do
     if [ -f $file ]; then
-      sed -i "s/github.com/nilsponsard/go-starter/$OUT/g" ./template.go 
-      sed -i "s/go-starter/$MOD_NAME/g" ./template.go
+      sed -i 's/github.com\/nilsponsard\/go-starter/'$MOD_URI'/g' $file
+      sed -i 's/go-starter/'$MOD_NAME'/g' $file
     fi
 done
 
+rm ./bootsrap.sh
